75d98c63af915289c8f93143bdde225f6384caf5
fricklerhandwerk <valentin@fricklerhandwerk.de>
2021-05-25 21:02:53 +0200

    set up python+selenium environment

---
+++ default.nix
@@ -0,0 +1,12 @@
+let
+  pkgs = import (
+    fetchTarball "https://github.com/NixOS/nixpkgs/archive/refs/heads/release-20.09.tar.gz"
+  ) {};
+  mach-nix = import (
+    fetchTarball "https://github.com/DavHau/mach-nix/archive/refs/tags/3.3.0.tar.gz"
+  ) { inherit pkgs; };
+in
+mach-nix.mkPythonShell {
+  requirements = builtins.readFile ./requirements.txt;
+  packagesExtra = with pkgs; [ firefox geckodriver ];
+}
