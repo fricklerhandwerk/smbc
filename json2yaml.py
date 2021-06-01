dc09619233506e9982a87e6d7bc2f68843a17db0
fricklerhandwerk <valentin@fricklerhandwerk.de>
2021-06-01 14:15:42 +0200

    fix access of nullable fields
    iterate over directories in-process, this is faster than spawning
    a process for each with `xargs`.
    
    make formatting of output string less magic. whitespace is hard.

--- json2yaml.py
+++ json2yaml.py
@@ -1,5 +1,4 @@
 import json
-import sys
 from pathlib import Path
 
 import yaml
@@ -10,23 +9,22 @@ def main():
     convert JSON to markdown YAML headers
     """
 
-    p = Path(sys.argv[1])
-    with open(p/'data.json', 'r') as f:
-        data = json.load(f)
-    # `width=` sets maximum line length
-    # https://stackoverflow.com/questions/18514205/how-to-prevent-yaml-to-dump-long-line-without-new-line/18526119#18526119
-    with open(str(p.resolve()) + '.md', 'w') as f:
-        f.write(markdown(yaml.dump(cleanup(data), width=float("inf"))))
+    for p in Path('comics').iterdir():
+        if p.is_dir():
+            with open(p/'data.json', 'r') as f:
+                data = json.load(f)
+            # `width=` sets maximum line length
+            # https://stackoverflow.com/questions/18514205/how-to-prevent-yaml-to-dump-long-line-without-new-line/18526119#18526119
+            with open(str(p.resolve()) + '.md', 'w') as f:
+                f.write(markdown(yaml.dump(cleanup(data), width=float("inf"))))
 
 
 def markdown(data):
     """
     write markdown YAML header
     """
-    return f"""
----
-{data}---
-"""
+    return f"---\n{data}---\n"
+
 
 def cleanup(data):
     """
@@ -42,9 +40,10 @@ def cleanup(data):
         'hovertext': data['hovertext'],
         'extra_image': data['extra_image'][image:],
         'id': data['url'][permalink:],
-        'prev': data['prev'][comic:],
-        'next': data['next'][comic:],
+        'prev': data['prev'][comic:] if data.get('prev') else None,
+        'next': data['next'][comic:] if data.get('next') else None,
     }
+
 
 if __name__ == "__main__":
     main()
